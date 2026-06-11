69da920d5dbcdcaf7b42a671c8e551414f1c1e78
Gideon <gideon.tobing@gmail.com>
2026-06-12 08:20:17 +1000

    feat: add CORS support for frontend

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,11 @@ import express from "express"; //imports the framework
 import morgan from "morgan"; //imports the logging middleware
 import applicationRoutes from "./routes/applications.js"; //imports the application routes
 import { errorHandler } from "./middleware/errorHandler.js";
+import cors from "cors";
 
 const app = express(); //creates an app instance
 app.use(express.json()); // middleware that parses JSON request bodies.
+app.use(cors());
 app.use(morgan("dev")); // middleware that logs HTTP requests in development format
 
 app.get("/health", (_req, res) => {
